25eb6cc1682c85ad7d00f6c7bbac6c4dee554c45
wannapg <reginakang52@gmail.com>
2019-10-08 13:35:18 +0900

    no message

--- temp/game.py
+++ temp/game.py
@@ -1,4 +1,7 @@
+
 import pygame
+import background
+from background import *
 from player import *
 from const import *
 
@@ -6,8 +9,8 @@ FRAME=0
 
 class Game:
     def __init__(self):
-        self.width=1000
-        self.height=640
+        self.width=900
+        self.height=600
         self.screen=pygame.display.set_mode((self.width,self.height))
         self.clock=pygame.time.Clock()
 
@@ -17,7 +20,7 @@ class Game:
 
         rect=self.screen.get_rect()
         player1=Player("girl/",(100,520))
-
+        background_=background(self.width,self.height)
 
         while True:
             time=self.clock.tick(60)
@@ -25,8 +28,8 @@ class Game:
             self.screen.fill((255,193,158))
 
             player1.update(self.screen)
+            background_.background(self.screen)
             pygame.display.flip()
-
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
