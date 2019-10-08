92db420bdf0c6f6192efdae5234492aa8f85c5f6
jjjinna <didwlssk2002@gmail.com>
2019-10-08 13:29:05 +0900

    Revert "no message"
    This reverts commit d382efc0e19c58e8b405494e0946a512f1d64076.

---
+++ temp/game.py
@@ -0,0 +1,38 @@
+import pygame
+from player import *
+from const import *
+
+FRAME=0
+
+class Game:
+    def __init__(self):
+        self.width=1000
+        self.height=640
+        self.screen=pygame.display.set_mode((self.width,self.height))
+        self.clock=pygame.time.Clock()
+
+    def main(self):
+        global FRAME
+        pygame.init()
+
+        rect=self.screen.get_rect()
+        player1=Player("girl/",(100,520))
+
+
+        while True:
+            time=self.clock.tick(60)
+            FRAME+=1
+            self.screen.fill((255,193,158))
+
+            player1.update(self.screen)
+            pygame.display.flip()
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit(0)
+
+if __name__=="__main__":
+    g=Game()
+    g.main()
