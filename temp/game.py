9f1d19fe00642edec569191a67336ee4b44160c8
jjjinna <didwlssk2002@gmail.com>
2019-10-20 22:42:22 +0900

    다이노,얼굴인식

--- temp/game.py
+++ temp/game.py
@@ -7,6 +7,7 @@ from item import *
 from trap import *
 from shot import *
 from button_detect import *
+from dino import *
 
 FRAME=0
 
@@ -58,6 +59,7 @@ class Game:
         self.clock=pygame.time.Clock()
         self.fire_rect=[530,40]
         self.BUTTON_ON=False
+        self.DINO_alive=True
 
     #key 입력에 따른 이벤트처리
     def event(self):
@@ -90,17 +92,20 @@ class Game:
         self.remove_platform_=pygame.sprite.Group()
         self.player_group=pygame.sprite.Group()
         self.button=pygame.sprite.Group()
+        self.dino_group=pygame.sprite.Group()
 
         pygame.init()
 
         #sprite 그룹에 추가할 sprite 선언
         self.player1=Player((self.width/2,self.height/2),self)
         self.button_=button_image(self)
+        self.dino_1=Dino(self,100,125) #100,125
 
         #sprite 그룹에 sprite 추가
         self.all_sprites.add(self.player1)
         self.player_group.add(self.player1)
         self.platforms.add(self.button_)
+        self.dino_group.add(self.dino_1)
 
         #배경 벽 불러옴
         for plat in PlatformList:
@@ -108,9 +113,8 @@ class Game:
             self.all_sprites.add(p)
             self.platforms.add(p)
 
-
         #초기화
-        trap1=trap(self)
+        #trap1=trap(self)
 
         for plat in remove_platform:
             p=platform_remove(*plat)
@@ -145,8 +149,10 @@ class Game:
                 #버튼 눌렸는지 확인
                 detect_button.detect(self.screen,self)
 
+                if self.DINO_alive==True:
+                    self.dino_1.update(self.screen)
                 self.shot_.shooting()
-
+                self.shot_.shoot_dino(self)
                 self.event()
                 self.all_sprites.update()
 
@@ -158,6 +164,8 @@ class Game:
                     self.player1.rect.left=0
 
                 self.all_sprites.draw(self.screen)
+                if self.BUTTON_ON==False:
+                    self.remove_platform_.draw(self.screen)
                 mouthOpen=face.face_recognition(self.screen)
                 item_.item_eat(self.screen,mouthOpen)
                 pygame.display.flip()
@@ -167,22 +175,3 @@ class Game:
                         pygame.quit()
                         exit(0)
 
-
-            #플레이어가 창 밖으로 나가지 못하게
-            if self.player1.rect.right>WIDTH:
-                self.player1.rect.right=WIDTH
-            if self.player1.rect.left<0:
-                self.player1.rect.left=0
-
-            self.all_sprites.draw(self.screen)
-
-            if self.BUTTON_ON==False:
-                self.remove_platform_.draw(self.screen)
-
-
-            pygame.display.flip()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit(0)
