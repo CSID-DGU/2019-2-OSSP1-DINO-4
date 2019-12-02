26fc8753f19cbaf5e95cc622535c6037456bfe15
wannapg <reginakang52@gmail.com>
2019-12-02 16:25:11 +0900

    .pyc

--- temp_scrolling/game.py
+++ temp_scrolling/game.py
@@ -6,11 +6,13 @@ from camera import *
 from background_platform import *
 from player import *
 from teleport import *
+from box import *
 from background import *
 from gameover import *
 from trap import *
 from shot import *
 from item import *
+from dino import *
 from button_detect import *
 from os import path
 from username import *
@@ -65,6 +67,14 @@ class Game:
 
         self.gameover=True
 
+        self.DINO_alive1=True
+        self.DINO_alive2=True
+
+        #상자 쳤는지 확인
+        self.box2_hit=False
+        self.box3_hit=False
+
+
     #high score data load
     def load_data(self):
         HS_FILE="highscore.txt"
@@ -116,23 +126,31 @@ class Game:
 
 
     def main(self):
-        global GAME_OVER_FIRE,GAME_OVER_ARROW
+        global GAME_OVER_FIRE,GAME_OVER_ARROW,GAME_OVER_MOVING_ARROW,GAME_END
         #spite_group 정의
         self.all_sprite=pygame.sprite.Group()
         self.player_sprite=pygame.sprite.Group()
         self.arrow_sprites=pygame.sprite.Group()
+        self.dino_group1=pygame.sprite.Group()
+        self.dino_group2=pygame.sprite.Group()
 
         #초기화
         FPS=30
         clock = pygame.time.Clock()
         self.background_=background() #sprite 아닌 background
         teleport_=teleport(self) #teleport
-        box_=box() #box
+        box_1=box1() #box1
+        box_2=box2() #box2
+        box_3=box3() #box3
         fire_bomb1=bomb(self) #폭탄1
         fire_bomb2=bomb(self) #폭탄2
         fire_bomb3=bomb(self) #폭탄3
         self.shot_=shot(self) #총알
+        self.moving_arrow_1=moving_arrow() #움직이는 창살
 
+        #공룡
+        self.dino_1=Dino(self,2500,290)
+        self.dino_2=Dino(self,3000,290)
 
         #아이템
         item1=item(self)
@@ -146,17 +164,30 @@ class Game:
         item9=item(self)
         item10=item(self)
         item11=item(self)
-
+        item12=item(self)
 
         #창살
         self.arrow_trap1=arrow(self,360,620,0)
         self.arrow_trap2=arrow(self,1840,1420,0)
-        #self.arrow_trap3=arrow(self,500,550)
-        #self.arrow_trap4=arrow(self,150,330)
+        self.arrow_trap3=arrow(self,1880,1420,0)
+        self.arrow_trap4=arrow(self,1920,1420,0)
+        self.arrow_trap5=arrow(self,1960,1420,0)
+        self.arrow_trap6=arrow(self,2000,1420,0)
+        self.arrow_trap7=arrow(self,2040,1420,0)
+        self.arrow_trap8=arrow(self,2080,1420,0)
+        self.arrow_trap9=arrow(self,2120,1420,0)
+        self.arrow_trap10=arrow(self,2160,1420,0)
+        self.arrow_trap11=arrow(self,2200,1420,0)
+        self.arrow_trap12=arrow(self,2240,1420,0)
+        self.arrow_trap13=arrow(self,2280,1420,0)
+        self.arrow_trap14=arrow(self,2320,1420,0)
 
         #버튼
         self.button_detect_1=button_detect() #버튼 바꼈는지 확인
         self.button1=button_image(self) #버튼 눌렸을 때 이미지 바꿔줌
+
+        #캐릭터 따라오는 불
+        self.fire_enemy=following_fire()
 
         #레벨,플레이어,배경sprite
         level = Level("level1")
@@ -164,6 +195,13 @@ class Game:
         self.world = level.world
         self.player = level.player
 
+
+        #sprite 그룹에 sprite 추가
+        self.arrow_sprites.add(self.arrow_trap1,self.arrow_trap2,self.arrow_trap3,self.arrow_trap4,self.arrow_trap5,self.arrow_trap6,\
+            self.arrow_trap7,self.arrow_trap8,self.arrow_trap9,self.arrow_trap10,self.arrow_trap11,self.arrow_trap12,self.arrow_trap13,self.arrow_trap14)
+        self.world.append(self.button1)
+        self.dino_group1.add(self.dino_1)
+        self.dino_group2.add(self.dino_2)
 
         #sprite 그룹에 sprite 추가
         self.arrow_sprites.add(self.arrow_trap1,self.arrow_trap2)
@@ -183,12 +221,12 @@ class Game:
         TEXT_COLOR=(0,0,0)
         BG_COLOR=(255,255,255)
 
-
         while True:
+            #print(self.player.rect.x,self.player.rect.y)
             #Gameover
-            if self.gameover or GAME_OVER_FIRE or GAME_OVER_ARROW:
-
-                self.user_name=username_.show_username_screen(self.score,self.dir)
+            if self.gameover or GAME_OVER_FIRE or GAME_OVER_ARROW or GAME_OVER_MOVING_ARROW or GAME_END:
+                if  GAME_OVER_FIRE or GAME_OVER_ARROW or GAME_OVER_MOVING_ARROW:
+                    self.user_name=username_.show_username_screen(self.score,self.dir)
                 gameover_.show_gameover_screen(self.score,self.dir)
                 #재초기화
                 self.up=False
@@ -197,10 +235,14 @@ class Game:
                 self.left=False
 
                 self.BUTTON_ON1=False
+                self.fire_enemy.first=True
 
                 self.all_sprite=pygame.sprite.Group()
                 self.player_sprite=pygame.sprite.Group()
                 self.arrow_sprites=pygame.sprite.Group()
+                self.dino_group1=pygame.sprite.Group()
+                self.dino_group2=pygame.sprite.Group()
+
 
                 #레벨,플레이어,배경sprite
                 level.create_level(0,0,self)
@@ -225,13 +267,23 @@ class Game:
                 item11=item(self)
 
                 #sprite 그룹에 sprite 추가
-                self.arrow_sprites.add(self.arrow_trap1,self.arrow_trap2)
+                self.arrow_sprites.add(self.arrow_trap1,self.arrow_trap2,self.arrow_trap3,self.arrow_trap4,self.arrow_trap5,self.arrow_trap6,\
+                    self.arrow_trap7,self.arrow_trap8,self.arrow_trap9,self.arrow_trap10,self.arrow_trap11,self.arrow_trap12,self.arrow_trap13,\
+                        self.arrow_trap14)
+                self.world.append(self.button1)
+                self.dino_group1.add(self.dino_1)
+                self.dino_group2.add(self.dino_2)
+
+                self.box2_hit=False
+                self.box3_hit=False
 
                 self.start_time=pygame.time.get_ticks()
                 self.gameover=False
-                GAME_OVER_FIRE=False
 
+                GAME_OVER_FIRE=False
+                GAME_OVER_MOVING_ARROW=False
                 GAME_OVER_ARROW=False
+                GAME_END=False
 
             #player 좌표 확인
             #print(self.player.rect.x,self.player.rect.y)
@@ -258,19 +310,51 @@ class Game:
                 teleport_.collide_detect(self)
 
             #box제어
-            box_.collide_detect(self,self.background_)
+            box_1.collide_detect(self,self.background_)
+            box_2.collide_detect(self,self.background_)
+            box_3.collide_detect(self,self.background_)
+
 
             #폭탄제어
             GAME_OVER_FIRE=fire_bomb1.bomb_draw(self,self.fire_rect1,3)
             GAME_OVER_FIRE=fire_bomb2.bomb_draw(self,self.fire_rect2,2.5)
             GAME_OVER_FIRE=fire_bomb3.bomb_draw(self,self.fire_Rect3,3.8)
 
+            #캐릭터 따라오는 불 제어
+            if self.player.rect.x>=2200 and self.player.rect.x<=3240 and self.player.rect.y>=560 and self.player.rect.y<=880:
+                GAME_OVER_FIRE=self.fire_enemy.follow(self,self.player.rect.x,self.player.rect.y)
+                self.fire_enemy.count+=1
+            else:
+                self.fire_enemy.first=True
+
             #버튼 제어
             self.button_detect_1.detect(self.screen,self)
             self.button1.button_draw(self)
 
+            #공룡제어
+            if self.DINO_alive1==True:
+                self.dino_1.update(self)
+            if self.DINO_alive2==True:
+                self.dino_2.update(self)
+
+
             #공격제어
             self.shot_.shooting()
+            self.shot_.shoot_dino1(self)
+            self.shot_.shoot_dino2(self)
+
+
+            #창살제어
+            if self.BUTTON_ON1 is True:
+                self.arrow_sprites.remove(self.arrow_trap3,self.arrow_trap4,self.arrow_trap5,self.arrow_trap6,self.arrow_trap8,self.arrow_trap9,\
+                self.arrow_trap10,self.arrow_trap11,self.arrow_trap12,self.arrow_trap13,self.arrow_trap14)
+
+            #움직이는 창살 제어
+            GAME_OVER_MOVING_ARROW=self.moving_arrow_1.moving_arrow_player_detect(self)
+            GAME_OVER_ARROW=self.arrow_trap1.arrow_player_detect()
+
+            #문 제어
+            GAME_END=self.background_.door_open(self)
 
             #창살제어
             GAME_OVER_ARROW=self.arrow_trap1.arrow_player_detect()
@@ -308,6 +392,9 @@ class Game:
             item9.draw_item(self,1900,1419,mouthOpen)
             item10.draw_item(self,2210,1210,mouthOpen)
             item11.draw_item(self,2550,1350,mouthOpen)
+            if self.box3_hit is True:
+                item12.draw_item(self,3160,360,mouthOpen)
+
 
 
             #print(mouthOpen)
